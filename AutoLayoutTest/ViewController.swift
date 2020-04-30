92a5be9ab8c84a50d7ff6de5a3903e968cab175f
David Pai <dbai2.0@gmail.com>
2020-04-30 17:46:16 +0800

    Initial Commit

---
+++ AutoLayoutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutTest
+//
+//  Created by David Pai on 2020/4/30.
+//  Copyright © 2020 Pai Bros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
